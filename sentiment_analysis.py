6f6d6f21e2841f2b1b23d1b07466acb5a85dda30
BrianLudwicki <68709490+BrianLudwicki@users.noreply.github.com>
2023-07-06 22:12:49 +0200

    second

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,2 +1,11 @@
-#git clone C:/Users/48783/Desktop/TRANSFORMERS/.git/
+#https://rogerdudler.github.io/git-guide/
+
+from transformers import pipeline
+import numpy as np
+import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import roc_auc_score, f1_score, confusion_matrix
+from sklearn.model_selection import train_test_split
 
